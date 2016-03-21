7f45c5489046cedf7d301ed21804669972793598
Jan Jouke Tjalsma <j.j.tjalsma@library.leidenuniv.nl>
2016-03-21 16:31:28 +0100

    Added first sheets

--- hack-de-bibliotheek-presentatie.js
+++ hack-de-bibliotheek-presentatie.js
@@ -1,23 +1,12 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.startup( function() {
+    Reveal.initialize({
+      loop: true,
+      controls: true,
+      progress: true,
+      transition: "linear",
+      "background-transition": "slide",
+      theme: "moon"
+    });
   });
 }
